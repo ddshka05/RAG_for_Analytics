43ca87d7520097c84d369254e99dc8cb994e3c3a
kseni-a-sh15 <ksh5552555@gmail.com>
2026-02-22 13:11:57 +0300

    Add chunk ID

--- chanking.py
+++ chanking.py
@@ -1,4 +1,6 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from read_pdf import data # обращаемся к созданному в другом файле словарю
+from os.path import splitext # библиотека для того, чтобы убрать расширения при ID
 
 text_splitter = RecursiveCharacterTextSplitter(
     chunk_size=300,
@@ -9,6 +11,13 @@ text_splitter = RecursiveCharacterTextSplitter(
 final = dict() #создаем словарь, куда будем сохранять ключи - названия документов, значения - чанки
 for company, text in data.items():
     chunks = text_splitter.create_documents(text)
+
+    # === Блок создания ID для чанков ===
+    company_name = splitext(company)[0] # Получаем название компании без расширения .pdf 
+    for idx, chunk in enumerate(chunks, 1): # Проходим по каждому чанку и добавляем ID
+        chunk_id = f"{company_name}-{idx}" # Формируем ID для каждого чанка
+        chunk.metadata["chunk_id"] = chunk_id # Добавляем айди в метаданные к чанку
+
     final[company]=chunks
 
 for i,j in final.items(): #тут я проверяла вывод чанков, что в итоге получается по кол-ву
@@ -31,3 +40,15 @@ for company, chunks in final.items():
     
     if len(chunks) > 3:
         print(f"\n... и ещё {len(chunks) - 3} чанков")
+
+
+# Примеры ID, которые выводятся
+count = 0
+for company, chunks in final.items():
+    for chunk in chunks:
+        if count >= 100:
+            break
+        count += 1
+        print(f"{count}. ID: {chunk.metadata['chunk_id']} | Длина: {len(chunk.page_content)}")
+    if count >= 100:
+        break
--- read_pdf.py
+++ read_pdf.py
@@ -23,17 +23,16 @@ def extract_text(pdf_data: bytes) -> str:
 
   return text
 
+data = {} # Создаем словарь для хранения данных по компаниям : {"Отчет_компании_1": [текст отчета], ...}
 
 with zipfile.ZipFile("DATA.zip", "r") as zip_data: # Открываем папку
   file_list = zip_data.namelist() # С помощью функции namelist() считываем называния файлов в папке в список
 
 
   for file_name in file_list:                # Проходимся циклом по каждому названию
+    clean_name = file_name.replace("Дата/", "") # Убираем название папки из имени компании
     pdf_data = zip_data.read(file_name)      # Считываем текст из PDF в папке в виде байтов
     text = extract_text(pdf_data)            # Пользуемся функцией для преобразования байтов в текст
-    data[file_name] = [text]                 # сохраняем полученный результат в словарь
+    data[clean_name] = [text]                 # сохраняем полученный результат в словарь
 
-
-data = {} # Создаем словарь для хранения данных по компаниям : {"Отчет_компании_1": [текст отчета], ...}
-
-print(*enumirate(data.keys(), 1), sep='\n') # Список ключей - названия файлов с отчетами
+print(*enumerate(data.keys(), 1), sep='\n') # Список ключей - названия файлов с отчетами
